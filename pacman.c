205f852c9ecc6ca3640d2786d307ffa9e835907b
davidl09 <114118694+davidl09@users.noreply.github.com>
2022-11-30 02:29:08 -0500

    Add files via upload

--- pacman.c
+++ pacman.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//#include <colours.h>
-
+#include <conio.h>
+#include "colours.h"
 #define PACMAN 'P'
 #define GHOST 'G'
 #define DOT '.'
@@ -28,7 +27,6 @@ char randomDir(char* ghostLocation, char* ptr[rows][cols], char dir);
 
 int main() {
     char* pointerToMap[rows][cols];
-
     for(int i = 0; i<rows; i++){
         for(int j = 0; j<cols; j++){
             (pointerToMap[i][j]) = malloc(sizeof(char));
@@ -38,7 +36,7 @@ int main() {
         }
     }
 
-    map = fopen("C:\\Users\\dalae\\CLionProjects\\pacman\\map.txt", "r");
+    map = fopen("C:\\Users\\dalae\\CLionProjects\\pacman\\cmake-build-debug\\map.txt", "r");
 
     for(int i = 0; i<rows; i++){
         for(int j = 0; j< cols; j++){
@@ -49,41 +47,45 @@ int main() {
     char* currentLocation = pointerToMap[5][5];
     char* ghostA = pointerToMap[1][1];
     char* ghostB = pointerToMap[9][9];
-    char dirA = RIGHT;//randomDir(ghostA, pointerToMap, UP);
-    char dirB = LEFT; //randomDir(ghostB, pointerToMap, DOWN);
-    printf("Printing map\n");
+    char dirA = RIGHT;
+    char dirB = LEFT;
     printMap(pointerToMap);
-    printf("Printed Map\n");
 
-    //the part after this needs to loop for the duration of the game.
+
 
     while(!winCheck(pointerToMap) && !loseCheck(pointerToMap)){
-        char input = (char)getchar();
+        char input = (char)getch();
         if(input != UP && input != LEFT && input != RIGHT && input != DOWN){
             continue;
         }else{
             dirA = ghostAlgo(ghostA, pointerToMap, dirA);
             dirB = ghostAlgo(ghostB, pointerToMap, dirB);
-
+            if(ghostA == ghostB){
+                dirB = randomDir(ghostB, pointerToMap, dirB);
+            }
             currentLocation = pacManLocation(currentLocation, pointerToMap, input);
-
-            printf("Direction of A is %c, direction of B is %c\n", dirA, dirB);
 
             ghostA = ghostMotion(ghostA, pointerToMap, dirA);
             ghostB = ghostMotion(ghostB, pointerToMap, dirB);
 
             winCheck(pointerToMap), loseCheck(pointerToMap);
-
+            system("cls");
             printMap(pointerToMap);
 
-            if (winCheck(pointerToMap) == 1) {
-                printf("Congratulations! You win! Press any key to exit the game\n");
-                break;
+            if (winCheck(pointerToMap)) {
+                colourChange(2);
+                printf("\n\nCongratulations! You win! Press any key to exit the game\n");
+                input = getch();
+                if(input)
+                    break;
             }
 
-            else if (loseCheck(pointerToMap) == 1) {
-                printf("Sorry, you lose. Press any key to exit the game\n");
-                break;
+            else if (loseCheck(pointerToMap)) {
+                colourChange(4);
+                printf("\n\nSorry, you lose. Press any key to exit the game\n");
+                input = getch();
+                if(input)
+                    break;
             }
         }
     }
@@ -99,7 +101,15 @@ int main() {
 void printMap(char* ptr[rows][cols]){
     for(int i = 0; i<rows; i++){
         for(int j = 0; j<cols; j++){
-            printf("%c  ", *ptr[i][j]);
+            if(*ptr[i][j] == GHOST)
+                colourChange(PINK);
+            else if(*ptr[i][j] == PACMAN)
+                colourChange(YELLOW);
+            else if(*ptr[i][j] == DOT)
+                colourChange(WHITE);
+            else colourChange(BLUE);
+            printf("%c ", *ptr[i][j]);
+
         }printf("\n");
     }printf("\n\n\n");
 }
@@ -166,7 +176,7 @@ char* pacManLocation(char* currentLocation, char* ptr[rows][cols], char input){
     else return currentLocation;
 }
 
-char randomDir(char* ghostLocation, char* ptr[rows][cols], char dir){
+char randomDir(char* ghostLocation, char* ptr[rows][cols], char dir){ //returns a legal (non-wall) direction, preferrably not opposite the given direction.
     int row, col;
     for (int i = 0; i < rows; i++) { //find the location of the ghost in terms of rows and columns.
         for (int j = 0; j < cols; j++) {
@@ -182,14 +192,14 @@ char randomDir(char* ghostLocation, char* ptr[rows][cols], char dir){
     char newDir;
     if(!isWall(ptr[row][col+1]) && dir != LEFT) {
         newDir = RIGHT;
-        printf("Random dir is right\n");
+        //printf("Random dir is right\n");
     }else if(!isWall(ptr[row+1][col]) && dir != UP){
         newDir = DOWN;
-        printf("Random dir is down\n");
+        //printf("Random dir is down\n");
     }else if(!isWall(ptr[row][col-1]) && dir != RIGHT) {
         newDir = LEFT;
-        printf("Random dir is left\n");
-    }else newDir = UP, printf("Random dir is up\n");
+        //printf("Random dir is left\n");
+    }else newDir = UP; //printf("Random dir is up\n");
 
     return newDir;
 }
@@ -207,12 +217,12 @@ char ghostAlgo(char* ghostLocation, char* ptr[rows][cols], char dir) {
             }
         }
     }
-    printf("Looking for pacman\n");
+    //printf("Looking for pacman\n");
     // The following block looks for pacman until it sees walls in all 4 directions. If it sees pacman it sets the direction accordingly.
-    for(int i = 1; dirUp == 1 || dirDown == 1 || dirLeft == 1 || dirRight == 1; i++){
+    for(int i = 1; dirUp == 1 || dirDown == 1 || dirLeft == 1 || dirRight == 1; i++){ // all 4 directions are marked as valid (1) until a wall is encountered (0).
 
         if(row-i > 0 && !isWall(ptr[row-i][col]) && dirUp == 1) {
-            printf("Looking up\n");
+            //printf("Looking up\n");
             dirUp = 1;
             if(*ptr[row-i][col] == PACMAN) {
                 dir = UP;
@@ -222,7 +232,7 @@ char ghostAlgo(char* ghostLocation, char* ptr[rows][cols], char dir) {
         else dirUp = 0;
 
         if(row+i < 11 && !isWall(ptr[row+i][col]) && dirDown == 1) {
-            printf("Looking down\n");
+            //printf("Looking down\n");
             dirDown = 1;
             if(*ptr[row+i][col] == PACMAN) {
                 dir = DOWN;
@@ -232,7 +242,7 @@ char ghostAlgo(char* ghostLocation, char* ptr[rows][cols], char dir) {
         else dirDown = 0;
 
         if(col-i > 0 && !isWall(ptr[row][col-i]) && dirLeft== 1) {
-            printf("Looking left\n");
+            //printf("Looking left\n");
             dirLeft = 1;
             if(*ptr[row][col-i] == PACMAN) {
                 dir = LEFT;
@@ -242,7 +252,7 @@ char ghostAlgo(char* ghostLocation, char* ptr[rows][cols], char dir) {
         else dirLeft = 0;
 
         if(col+i < 11 &&!isWall(ptr[row][col+i]) && dirRight == 1) {
-            printf("Looking right\n");
+            //printf("Looking right\n");
             dirUp = 1;
             if(*ptr[row][col+i] == PACMAN) {
                 dir = RIGHT;
@@ -253,31 +263,31 @@ char ghostAlgo(char* ghostLocation, char* ptr[rows][cols], char dir) {
     }
 
     
-    printf("Pacman not found, trying %c\n", dir);
+    //printf("Pacman not found, trying %c\n", dir);
 
 
     if(dir == UP && !isWall(ptr[row - 1][col])) {
-        printf("Going up\n");
+        //printf("Going up\n");
         dir = UP;
         return dir;
     }
     else if(dir == DOWN && !isWall(ptr[row + 1][col])){
-        printf("Going down\n");
+        //printf("Going down\n");
         dir = DOWN;
         return dir;
     }
     else if(dir == RIGHT && !isWall(ptr[row][col +1])) {
-        printf("Going right\n");
+        //printf("Going right\n");
         dir = RIGHT;
         return dir;
     }
     else if(dir == LEFT && !isWall(ptr[row][col-1])) {
-        printf("Going left\n");
+        //printf("Going left\n");
         dir = LEFT;
         return dir;
     }
     else{
-        printf("Trying random\n");
+        //printf("Trying random\n");
         dir = randomDir(ghostLocation, ptr, dir);
         return dir;
     }
@@ -297,35 +307,35 @@ char* ghostMotion(char* ghostLocation, char* ptr[rows][cols], char dir){
         }
     }
 
-    printf("Ghost is at %d, %d\n", row, col);
+    //printf("Ghost is at %d, %d\n", row, col);
 
     if(dir == UP && !isWall(ptr[row-1][col])){
-        if(*ptr[row-1][col] == EMPTY || *ptr[row-1][col] == PACMAN)
+        if(*ptr[row-1][col] == EMPTY || *ptr[row-1][col] == PACMAN || *ptr[row-1][col] == GHOST)
             *ptr[row][col] = EMPTY;
         else *ptr[row][col] = DOT;
         row--;
         *ptr[row][col] = GHOST;
     }else if(dir == DOWN && !isWall(ptr[row+1][col])) {
-        if(*ptr[row+1][col] == EMPTY || *ptr[row+1][col] == PACMAN)
+        if(*ptr[row+1][col] == EMPTY || *ptr[row+1][col] == PACMAN || *ptr[row+1][col] == GHOST)
             *ptr[row][col] = EMPTY;
         else *ptr[row][col] = DOT;
         row++;
         *ptr[row][col] = GHOST;
     }else if(dir == LEFT && !isWall(ptr[row][col-1])) {
-        if(*ptr[row][col-1] == EMPTY || *ptr[row][col-1] == PACMAN)
+        if(*ptr[row][col-1] == EMPTY || *ptr[row][col-1] == PACMAN || *ptr[row][col-1] == GHOST)
             *ptr[row][col] = EMPTY;
         else *ptr[row][col] = DOT;
         col--;
         *ptr[row][col] = GHOST;
     }else if(dir == RIGHT && !isWall(ptr[row][col+1])) {
-        if(*ptr[row][col+1] == EMPTY || *ptr[row][col+1] == PACMAN)
+        if(*ptr[row][col+1] == EMPTY || *ptr[row][col+1] == PACMAN || *ptr[row][col+1] == GHOST)
             *ptr[row][col] = EMPTY;
         else *ptr[row][col] = DOT;
         col++;
         *ptr[row][col] = GHOST;
-    }else printf("Direction passed to motion fx was not valid\n");
+    }else return ghostMotion(ghostLocation, ptr, randomDir(ghostLocation, ptr, dir));//printf("Direction passed to motion fx was not valid\n");
 
-    printf("Ghost moved to %d, %d\n", row, col);
+    //printf("Ghost moved to %d, %d\n", row, col);
     return ptr[row][col];
 }
 
@@ -366,3 +376,7 @@ int isGhost(char* ptr){
         return 1;
     else return 0;
 }
+
+//int colourChange(int colour){
+
+//}
